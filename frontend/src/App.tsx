6ab9cfa6e32857ea07077366fa50f612b98f45d9
Chandan Pandit <125503076+Chandanpandit80029@users.noreply.github.com>
2026-07-01 16:53:33 +0530

    Add BrowserRouter to routing imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter , Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { useEffect, useState } from 'react';
